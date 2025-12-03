71fe72cf81a3d97334db0a76cb46964dd949439b
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-12-03 01:05:32 +0000

    removed default

--- terraform-rest-api/vars.tf
+++ terraform-rest-api/vars.tf
@@ -1,22 +1,18 @@
 # the following uses default for quick plan and apply without a pipeline
 variable "region" {
   type    = string
-  default = "eu-west-2"
 }
 
 variable "project_name" {
   type    = string
-  default = "terraform-rest-api"
 }
 
 # https://whatismyipaddress.com/
 variable "home_ip" {
   type        = string
   description = "ip allowed to call API gateway"
-  default     = "143.58.253.75"
 }
 
 variable "environment" {
   type    = string
-  default = "dev"
 }
